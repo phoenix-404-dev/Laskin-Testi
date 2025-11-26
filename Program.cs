aabdbff5c1b1bf55e149f0aa9d78b8155b925fb4
phoenix-404-dev <sc13907@student.samk.fi>
2025-11-26 18:46:18 +0200

    Laskin TEST

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Tervetuloa Ahmedin laskimeen!");
+
+Console.WriteLine("Anna ensimmäinen luku!"); 
+float luku1 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Anna toinen luku!"); 
+float luku2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Valitse operaatio: +, -, *, /"); 
+string  operaatio = Console.ReadLine();
+
+if ( operaatio == "+") 
+{ 
+    Console.WriteLine($"Tulos on: {luku1 + luku2}"); 
+} 
+else if (operaatio == "-") 
+{ 
+    Console.WriteLine($"Tulos on: {luku1 - luku2}"); 
+} 
+else if (operaatio == "*") 
+{ 
+    Console.WriteLine($"Tulos on: {luku1 * luku2}"); 
+} 
+else if (operaatio == "/") 
+{
+    if (luku2 == 0)
+    {
+        Console.WriteLine("Perkeleeee, nollalla ei voi jakaa!"); 
+    }
+    else
+    {
+        Console.WriteLine($"Tulos on: {luku1 / luku2}");
+    }
+} 
